e86082b2e823cb0235482b7deb63c137f6346d2b
AppOfficer <73411359+AppOfficer@users.noreply.github.com>
2023-01-05 15:21:08 +0800

    New project for new version!

---
+++ LGmain.cpp
@@ -0,0 +1,18 @@
+/* This is the MAIN program of LocalGen.                 */
+/* Copyright (c) 2023 LocalGen-dev; All rights reserved. */
+
+/* Internet base */
+#include "LGweb.hpp"
+/* printings */
+#include "LGprint.hpp"
+/* game options */
+#include "LGgame.hpp"
+/* pages */
+#include "LGpages.hpp"
+
+/*************** the main function ***************/
+
+signed main(signed argc, char** argv) {
+	MainPage();
+	exit(0);
+}
